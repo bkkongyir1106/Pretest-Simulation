cc09a10cb09addeded6939f70a7d49b180a2c9ee
Benedict Kongyir <70083853+bkkongyir1106@users.noreply.github.com>
2023-12-06 13:11:12 -0600

    density plots of test statistic under H1

---
+++ Power and Type I error Rate/sim.R
@@ -0,0 +1,28 @@
+
+num.sim <- 1e5
+n <- 10
+df <- 5
+deltavec <- c(0, 1 ,2, 3)
+tstat <- tstaty <- matrix(NA, nrow = num.sim, ncol = length(deltavec))
+
+set.seed(123)
+
+for(i in 1:num.sim){
+  for (k in 1:length(deltavec)){
+    x <- (rchisq(n, df) - df)/sqrt(2*df) + deltavec[k]
+    y <- rnorm(n) + deltavec[k]
+    tstat[i, k] <- sqrt(n)*mean(x)/sd(x)
+    tstaty[i, k] <- sqrt(n)*mean(y)/sd(y)
+  }
+}
+
+plot(density(tstat[, 1]), typ = "n", xlim = c(-5, 15))
+legend_text <- c()
+for (k in 1:length(deltavec)){
+  lines(density(tstaty[, k]), col = "red", lty = k)
+  lines(density(tstat[, k]), col = "blue", lty = k)
+  legend_text <- c(legend_text, col = k) 
+}
+legend('topright', legend = c("chi-squared", "normal"), col=c("red", "blue"))
+
+
