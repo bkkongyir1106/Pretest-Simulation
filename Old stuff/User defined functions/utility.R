b4d7fb309fa28a2774d6700d1e266c4e1fd192e8
Benedict Kongyir <kongyirbenkk@gmail.com>
2024-05-15 07:57:39 -0500

    Compressed Folders
    Old stuff folder contains previous work such as replication of simulation of the paper "to test of not to test", Distribution of test statistics, and document files.
    Expected Power loss and Expected Inflation of Type I error rate folder contains current work

---
+++ Old stuff/User defined functions/utility.R
@@ -0,0 +1,13 @@
+#%%%%%%%%%%%%%%%%%%%%% (1) UTILS %%%%%%%%%%%%%%%%%%%%%%%----
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load(MASS, doMC, microbenchmark, psych, kernlab, tidyverse, haven, foreach, doParallel, doRNG,
+               Matrix, latex2exp, doSNOW, nortest, dgof, moments, stats, tseries, LaplacesDemon, VGAM, openxlsx)
+
+## import utility files
+path = "~/Documents/Testing Files/ExpectileGEE"                  # set path
+file.sources = list.files(path,                                  # locate all .R files
+                          pattern="*.R$", full.names=TRUE, 
+                          ignore.case=TRUE)
+sapply(file.sources,source,.GlobalEnv)
+
+
